9a6319d59f103378c7845bf6860227546b8cca41
Sir Mino <115249835+SirVoly@users.noreply.github.com>
2025-08-29 10:44:26 +0000

    Added base for the list command

--- internal/handlers/listBooks.go
+++ internal/handlers/listBooks.go
@@ -1,2 +1,21 @@
 package handlers
 
+import (
+	"context"
+	"fmt"
+
+	"github.com/SirVoly/bookkeeper/internal/database"
+)
+
+func ListBooksHandler(_ []string) error {
+	queries, err := database.InitDatabase()
+	if err != nil {
+		return fmt.Errorf("error getting database: %v", err)
+	}
+	books,err := queries.ListBooks(context.Background())
+	if err != nil {
+		return fmt.Errorf("error listing books: %v", err)
+	}
+	fmt.Println(books)
+	return nil
+}
